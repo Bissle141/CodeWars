3fbdd3ec65529b6e7012c8a87e5da9290408e6c2
Alyssa <alyssa.black141@gmail.com>
2023-03-12 22:19:30 -0600

    longest common prefix

---
+++ Challenges/Longest_Common_Prefix.py
@@ -0,0 +1,13 @@
+def longestCommonPrefix(strs):
+    """
+    :type strs: List[str]
+    :rtype: str
+    """
+    res = ''
+
+    for i in range(len(strs[0])):
+        for j in strs:
+            if i == len(j) or j[i] != strs[0][i]:
+                return res
+        res += strs[0][i]
+    return res
